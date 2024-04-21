96057a33708a04f372e46e8ac0b9eaff4d5d30e3
Bono <kubonkwan99@gmail.com>
2024-04-21 16:27:57 +0800

    feat: setup apis

--- src/apis/map.ts
+++ src/apis/map.ts
@@ -1,61 +1,55 @@
 import { directionsApi, mockApi } from "@/lib/axios";
 import { delay } from "@/utils";
 
+import type { FetchPathResponse } from "@/types";
 import type { AxiosError } from "axios";
 
-export const getPaths = async (origin: string, destination: string) => {
+export const getPaths = async (
+  origin: string,
+  destination: string,
+  callback: (data: FetchPathResponse) => void
+) => {
   try {
     const response = await mockApi.post("/route", {
       origin,
       destination,
     });
-    // const response = await mockApi.post("/mock/route/success");
     const token = response.data.token;
-    return await fetchPathWithToken(token);
+    return await fetchPathWithToken(token, callback);
   } catch (error) {
-    console.error("Error getting token or fetching data:", error);
+    console.error("Error getting token or fetching data: ", error);
     throw error;
   }
 };
 
-type FetchPathResponse = {
-  status: "in progress" | "success";
-  path?: number[][];
-};
-
 export const fetchPathWithToken = async (
   token: string,
+  callback: (data: FetchPathResponse) => void,
   retries = 10,
   interval = 2000
-  // TODO: fix return type
-): Promise<{
-  status: "in progress" | "success" | "failure";
-  path?: number[][];
-  total_distance?: number;
-  total_time?: number;
-}> => {
+): Promise<FetchPathResponse> => {
   try {
     const res = await mockApi.get<FetchPathResponse>(`/mock/route/success`);
     // const res = await mockApi.get<FetchPathResponse>(`/route/${token}`);
-    // const res = await mockApi.get<FetchPathResponse>(`/mock/route/500`);
-    if (res.status === 500) {
-      throw new Error("Server error");
-    }
+    // const res = await mockApi.get<FetchPathResponse>(`/mock/route/inprogress`);
+    callback(res.data);
+
     if (res.data.status === "in progress" && retries > 0) {
       await delay(interval);
       console.log("Retrying...");
-      return await fetchPathWithToken(token, retries - 1);
-    } else if (res.data.status === "success") {
-      console.log(res);
-
+      return await fetchPathWithToken(token, callback, retries - 1);
+    }
+    if (res.data.status === "failure") {
       return res.data;
-      // return res.data.path as number[][];
+    }
+    if (res.data.status === "success") {
+      return res.data;
     }
     throw new Error("Path fetching failed or status is not recognized.");
   } catch (error: unknown) {
     if (retries > 0 && (error as AxiosError).response?.status !== 500) {
       await delay(interval);
-      return await fetchPathWithToken(token, retries - 1);
+      return await fetchPathWithToken(token, callback, retries - 1);
     }
     console.error("Error fetching data:", error);
     throw error;
